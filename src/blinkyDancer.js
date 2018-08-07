41da7564d7632d388d302cd27b7717153c644f44
thekelvintw <kelvin.aiesec@gmail.com>
2018-08-06 17:46:56 -0700

    show blinky dancer

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,4 +1,4 @@
-var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+/*var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
@@ -16,4 +16,49 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
   };
 
   return blinkyDancer;
+};
+
+*/
+var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  //var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  dancer.call(this, top, left, timeBetweenSteps);
+  //var oldStep = makeDancer.prototype.step.bind(this);
+  
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+/*
+  blinkyDancer.step = function() {
+    // call the old version of step at the beginning of any call to this new version of step
+    oldStep();
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    blinkyDancer.$node.toggle();
+  };*/
+this.step.apply(this)
+// this.step()
+  //this.setPosition(this.top, this.left);
+
+//  return blinkyDancer;
+console.log(this)
+  
+};
+
+makeBlinkyDancer.prototype = Object.create(dancer.prototype);
+makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
+
+
+
+
+// step = function() {
+makeBlinkyDancer.prototype.step = function() {
+  var oldStep = dancer.prototype.step.bind(this);
+  // call the old version of step at the beginning of any call to this new version of step
+  oldStep();
+  // toggle() is a jQuery method to show/hide the <span> tag.
+  // See http://api.jquery.com/category/effects/ for this and
+  // other effects you can use on a jQuery-wrapped html tag.
+  //blinkyDancer.$node.toggle();
+  this.$node.toggle();
 };
